75de310bc3b9f4dc6d382d8322553158af19a7e9
Ivan Fraixedes <ifcdev@gmail.com>
2015-07-26 11:47:00 +0100

    Added rest of implementaiton to show the images captured by thermal camera

--- app/src/main/java/hack/mrowlsafari/CaptureActivity.java
+++ app/src/main/java/hack/mrowlsafari/CaptureActivity.java
@@ -1,29 +1,37 @@
 package hack.mrowlsafari;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.flir.flironesdk.Device;
 import com.flir.flironesdk.Frame;
 import com.flir.flironesdk.FrameProcessor;
 import com.flir.flironesdk.RenderedImage;
 
+import java.nio.ByteBuffer;
 import java.util.EnumSet;
 
 
 public class CaptureActivity extends Activity implements Device.Delegate, Device.StreamDelegate, FrameProcessor.Delegate {
 	private FrameProcessor mProcessor;
+	private ImageView thermalImageView;
+	private Bitmap thermalBitmap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_capture);
 
+		thermalImageView = (ImageView) findViewById(R.id.thermalView);
+
 		RenderedImage.ImageType blendedType = RenderedImage.ImageType.BlendedMSXRGBA8888Image;
 		mProcessor = new FrameProcessor(this, this, EnumSet.of(blendedType));
 		mProcessor.setImagePalette(RenderedImage.Palette.Iron);
+
 	}
 
 	@Override
@@ -69,6 +77,17 @@ public class CaptureActivity extends Activity implements Device.Delegate, Device
 
 	@Override
 	public void onFrameProcessed(RenderedImage renderedImage) {
-
+		if (this.thermalBitmap == null
+			|| (renderedImage.width() != thermalBitmap.getWidth()
+			|| renderedImage.height() != thermalBitmap.getHeight())) {
+			thermalBitmap = Bitmap.createBitmap(renderedImage.width(), renderedImage.height(), Bitmap.Config.ARGB_8888);
+			thermalBitmap.copyPixelsToBuffer(ByteBuffer.wrap(renderedImage.pixelData()));
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					thermalImageView.setImageBitmap(thermalBitmap);
+				}
+			});
+		}
 	}
 }
